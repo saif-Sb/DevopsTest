1f66df0b9c263dc04afeead57eb993d69846cb86
saif-Sb <75327168+saif-Sb@users.noreply.github.com>
2022-01-26 11:09:55 -0800

    SPFX Webpart

---
+++ src/webparts/helloDevops/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IHelloDevopsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'HelloDevopsWebPartStrings' {
+  const strings: IHelloDevopsWebPartStrings;
+  export = strings;
+}
