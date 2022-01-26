42b1c4ce39765d28d4e509f07e0831cc248763d3
saif-Sb <75327168+saif-Sb@users.noreply.github.com>
2022-01-26 11:22:28 -0800

    update content

--- src/webparts/helloDevops/components/HelloDevops.tsx
+++ src/webparts/helloDevops/components/HelloDevops.tsx
@@ -10,7 +10,7 @@ export default class HelloDevops extends React.Component<IHelloDevopsProps, {}> 
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
+              <span className={ styles.title }>Welcome to SharePoint Neos!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
