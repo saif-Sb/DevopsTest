d47bd000d5e78f162ef9e5ee65320b65c260d623
Saif sb <saifeddine.sibouih@gmail.com>
2022-04-11 14:45:31 +0200

    test feat

--- src/webparts/helloDevops/components/HelloDevops.tsx
+++ src/webparts/helloDevops/components/HelloDevops.tsx
@@ -6,11 +6,11 @@ import { escape } from '@microsoft/sp-lodash-subset';
 export default class HelloDevops extends React.Component<IHelloDevopsProps, {}> {
   public render(): React.ReactElement<IHelloDevopsProps> {
     return (
-      <div className={ styles.helloDevops }>
+      <div className={ styles.helloDevops}>
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint Neos!</span>
+              <span className={ styles.title }>Welcome to SharePoint Saif!</span>
               <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
